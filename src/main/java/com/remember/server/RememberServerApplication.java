316dd14ece76cad41b3a67f9e09c0378a67b8309
HelloWOrld4625 <HelloWOrld4625@outlook.com>
2015-12-19 15:49:11 +0900

    기본적인 Thymeleaf 설정 추가

--- src/main/java/com/remember/server/RememberServerApplication.java
+++ src/main/java/com/remember/server/RememberServerApplication.java
@@ -3,14 +3,24 @@ package com.remember.server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.thymeleaf.extras.java8time.dialect.Java8TimeDialect;
 
 //@EnableAutoConfiguration
 @SpringBootApplication
+@EnableWebMvc
 //@ComponentScan(basePackages = "com.remember.server.*")
 public class RememberServerApplication {
+
+	@Bean
+	public Java8TimeDialect java8TimeDialect() {
+		return new Java8TimeDialect();
+	}
 
     public static void main(String[] args) {
         SpringApplication.run(RememberServerApplication.class, args);
     }
+
 }
---
+++ src/main/java/com/remember/server/controller/ThymeLeafTestController.java
@@ -0,0 +1,35 @@
+package com.remember.server.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.time.LocalDateTime;
+
+@Controller
+public class ThymeLeafTestController {
+
+    @RequestMapping("/greeting")
+    public String greeting(
+            @RequestParam(value="name", required=false, defaultValue="World") String name,
+            Model model
+    ) {
+        model.addAttribute("name", name);
+        return "greeting";
+    }
+
+	@RequestMapping("/")
+	String index(Model model) {
+		model.addAttribute("now", LocalDateTime.now());
+		return "index";
+	}
+
+	@RequestMapping("properties")
+	@ResponseBody
+	java.util.Properties properties() {
+		return System.getProperties();
+	}
+
+}
