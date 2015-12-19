73c695ef27d6e1c283daf6aa39e57050d864b477
HelloWOrld4625 <HelloWOrld4625@outlook.com>
2015-12-20 03:11:23 +0900

    Main Subscribed Contents endpoint 완성

--- src/main/java/com/remember/server/service/SubscriptionService.java
+++ src/main/java/com/remember/server/service/SubscriptionService.java
@@ -7,11 +7,21 @@ import com.remember.server.repository.IssueRepository;
 import com.remember.server.repository.UserRepository;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.BasicQuery;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.CriteriaDefinition;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Service;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
+import java.util.function.Supplier;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static org.springframework.data.mongodb.core.query.Criteria.where;
+import static org.springframework.data.mongodb.core.query.Query.query;
 
 /**
  * Created by NerdHerd on 2015. 12. 20..
@@ -25,6 +35,9 @@ public class SubscriptionService {
 
 	@Autowired
 	private UserRepository userRepository;
+
+	@Autowired
+	private MongoTemplate mongoTemplate;
 
 	public void subscribe(ObjectId issueOid, UserEntity userEntity) {
 
@@ -61,4 +74,49 @@ public class SubscriptionService {
 		userEntity = userRepository.findOne(userEntity.getId());
 		return userEntity.getSubscribedIssues();
 	}
+
+	public List<IssueEntity> getMainSubscribedIssues(UserEntity userEntity) {
+		userEntity = userRepository.findOne(userEntity.getId());
+
+		List<IssueEntity> result = userEntity.getSubscribedIssues().stream().limit(10).collect(Collectors.toList());
+
+		if (result.size() < 10)
+			result.addAll(getRandomIssues(result, 9));
+
+		return result;
+	}
+
+	private List<IssueEntity> getRandomIssues(List<IssueEntity> issueEntities, int size) {
+
+		List<IssueEntity> allIssueEntities = issueRepository.findAll();
+
+		LinkedList<IssueEntity> result = new LinkedList<>();
+
+		HashSet<ObjectId> dupSet = new HashSet<>();
+		dupSet.addAll(issueEntities.stream().map(IssueEntity::getId).collect(Collectors.toList()));
+
+		int count = 10;
+		while (result.size() < size && count > 0) {
+			IssueEntity newIssueEntity = getRandomIssue(allIssueEntities);
+
+			if (dupSet.contains(newIssueEntity.getId())) {
+				count--;
+				continue;
+			}
+
+			dupSet.add(newIssueEntity.getId());
+			result.addLast(newIssueEntity);
+
+		}
+
+		return result;
+	}
+
+	private IssueEntity getRandomIssue(List<IssueEntity> issueEntities) {
+
+		Random randomizer = new Random();
+		return issueEntities.get(randomizer.nextInt(issueEntities.size()));
+
+	}
+
 }
