25ebe70879519e9c7641faf727ca10cef774de7f
HelloWOrld4625 <HelloWOrld4625@outlook.com>
2015-12-20 00:53:22 +0900

    IssueEntity -> IssueModel PropertyMapping 추가됨

---
+++ src/main/java/com/remember/server/config/modelmapper/IssueEntityToIssueModelPropertyMap.java
@@ -0,0 +1,44 @@
+package com.remember.server.config.modelmapper;
+
+import com.github.jmnarloch.spring.boot.modelmapper.PropertyMapConfigurerSupport;
+import com.remember.server.entity.IssueEntity;
+import com.remember.server.entity.TagEntity;
+import com.remember.server.model.IssueModel;
+import org.modelmapper.AbstractConverter;
+import org.modelmapper.Converter;
+import org.modelmapper.PropertyMap;
+import org.modelmapper.spi.MappingContext;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+/**
+ * Created by NerdHerd on 2015. 12. 20..
+ */
+
+@Configuration
+public class IssueEntityToIssueModelPropertyMap extends PropertyMapConfigurerSupport<IssueEntity, IssueModel> {
+
+	private Converter<List<TagEntity>, List<String>> tagEntitiesConverter = new AbstractConverter<List<TagEntity>, List<String>>() {
+		@Override
+		protected List<String> convert(List<TagEntity> source) {
+			return source.stream().map(TagEntity::getName).collect(Collectors.toList());
+		}
+	};
+
+	@Override
+	public PropertyMap<IssueEntity, IssueModel> mapping() {
+		return new PropertyMap<IssueEntity, IssueModel>() {
+			@Override
+			protected void configure() {
+				map().setContent(source.getContent());
+				map().setCreatedAt(source.getCreatedAt());
+				map().setSubject(source.getTitle());
+				using(tagEntitiesConverter).map(source.getTags()).setTags(null);
+				skip().setTimeTreeId(null);
+			}
+		};
+	}
+}
